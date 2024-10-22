5dbfb90cd2acc0b7039f91a2fb32b90d7b6a0b5a
sachineleven <sachinjeph58@gmail.com>
2024-10-23 03:23:04 +0530

    variable animation steps based on cursor speed

--- cursor-identifier.py
+++ cursor-identifier.py
@@ -194,10 +194,13 @@ for frame_num, position, speed in cursor_data:
             # Smoothly interpolate zoom levels via n no. of zoom steps
             #we only want smooth transition when zoom level has changed, since adding zoom steps increases video size, we dont want to increase the video size needlessly and increasing zoom steps also slows down the video between those frames
             #I think levels should maybe according to the cursor speed, so we should normalize it for cursor speed
-            zoom_steps = 3
-            print("Zooming animation ->",speed/zoom_steps)
-            if prev_zoom_level != target_zoom_level:
-                zoom_steps = 10
+            #Assuming 300 is a good speed smooth and understandably cursor speed
+            good_cursor_speed = 100#in pixels per frame
+            zoom_steps = int(speed/good_cursor_speed)
+            # zoom_steps = 3
+            print("Zooming animation steps->",zoom_steps)
+            # if prev_zoom_level != target_zoom_level:
+            #     zoom_steps = 10
             zoom_levels = smooth_zoom(prev_zoom_level, target_zoom_level, steps=zoom_steps)
 
             # Apply zoom for each interpolated zoom level
