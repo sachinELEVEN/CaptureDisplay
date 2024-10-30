bee0f7f0f346550f49a979b881747fdd91f06092
sachineleven <sachinjeph58@gmail.com>
2024-10-31 03:26:03 +0530

    bug fix

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 def get_resource_path(relative_path):
         #Paths change after creating an artifact using pyinstaller.
@@ -12,6 +13,15 @@ def get_resource_path(relative_path):
 
         # globalSpace.append_to_logs("IN PY_INSTALLER",sys._MEIPASS)
         is_prod = True
-        #since prod builds are done from ./build-pipeline that is our base path, we need to go one level down from that so that we can access other resources in normal fashion from other files during development like the assets folder
-        relative_path = '../' + relative_path  if is_prod else relative_path
+        #since prod builds are done from ./build-pipeline that is our base path, we need to remove build-pipeline so that we can access other resources in normal fashion from other files during development like the assets folder
+        base_path = modify_path(base_path,'build-pipeline')
         return os.path.join(base_path, relative_path)
+
+def modify_path(path, part_to_remove):
+    # Normalize the path to remove any redundant relative parts (like "../" or "./")
+    normalized_path = os.path.normpath(path)
+    
+    # Remove the specified part using regex
+    modified_path = re.sub(rf'/{re.escape(part_to_remove)}', '', normalized_path, count=1)
+    
+    return modified_path
